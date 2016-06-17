c048a3122f1f7e648ae08319f73893ec2ac90462
me <russell@dentalemr.com>
2016-06-16 18:17:06 -0700

    Enhanced to be able to deal with xml named array elements (e.g. 	<e> <a>text</a> <a>text</a> </e>	"e": { "a": ["text", "text"] })

--- dist/angular-treeviewer.js
+++ dist/angular-treeviewer.js
@@ -43,17 +43,21 @@
                         };
 
                         scope.toggleControls = function(thing) {
-                            if (thing instanceof Array && thing.length === 0) {
+                            if  (
+                                    !thing //null, undefined, false, 0, '', NaN
+                                    || (typeof thing !== 'object') //boolean, number, string, symbol, function
+                                    || (thing instanceof Array && thing.length === 0) //zero length array
+                                ) {
                                 return;
                             }
-
+                            // if: non-null object or array with a size greater than zero. 
                             thing._childrenVisible = !thing._childrenVisible;
                             thing._iconClass = thing._childrenVisible ? scope.openIconClass : scope.closeIconClass;
                         };
 
                         scope.setControls = function(thing) {
                             if (typeof thing === 'object') {
-                                if (typeof thing._iconClass === 'undefined') {
+                                if (thing && typeof thing._iconClass === 'undefined') {
                                     if (thing instanceof Array && thing.length === 0) {
                                         thing._iconClass = scope.emptyIconClass;
                                     } else {
@@ -78,25 +82,32 @@
                         scope.getRepeatClass = function(thing, isRoot) {
                             if (!isRoot) {
                                 if (typeof thing === 'object') {
-                                    return "expanded-item-with-icon";
+                                    return 'expanded-item-with-icon';
                                 } else {
-                                    return "expanded-item-without-icon";
+                                    return 'expanded-item-without-icon';
                                 }
                             } else {
                                 if (typeof thing !== 'object') {
-                                    return "root-item-without-icon";
+                                    return 'root-item-without-icon';
                                 }
                             }
                         };
 
-                        scope.search = "";
+                        scope.search = '';
 
                         scope.$watch('search', function (val) {
                             if (!scope.flattenedTree || scope.flattenedTree.length == 0) {
                                 scope.flattenedTree = [];
-                                flatten(scope.fullTree, scope.flattenedTree);
+                                flatten(scope.heirarchicalTree, scope.flattenedTree);
                             }
-                            scope.displayTree = val.length > 0 ? scope.flattenedTree : scope.fullTree;
+                            scope.displayTree = val.length > 0 ? scope.flattenedTree : scope.heirarchicalTree;
+                        });
+
+                        scope.$watch('fullTree', function (val) {
+                            scope.search = ''; //reset search
+                            scope.flattenedTree = null; //clear flattened tree
+                            scope.heirarchicalTree = preProcessTree(val, scope.removeEmptyValues, scope.normalizeWords);
+                            scope.displayTree = scope.heirarchicalTree;
                         });
 
                         function flatten(node, arr, val, parent) {
@@ -114,13 +125,15 @@
                                 return;
                             } else if (node && ((typeof node === 'string') || (typeof node === 'boolean') || (typeof node === 'number')) && val) {
                                 var par = '';
-                                for (var key in parent) {
+                                Object.getOwnPropertyNames(parent).forEach(function(key, idx, array) {
                                     if (parent[key] && typeof parent[key] === 'string') {
-                                        par += key + ": " + parent[key] + '; ';
+                                        if (key !== '_childrenVisible' && key !== '_iconClass' && key !=='$$hashKey') {
+                                            par += key + ': ' + parent[key] + '; ';
+                                        }
                                     }
-                                }
+                                });
                                 var str = par;
-                                if (arr.indexOf(str) == -1) {
+                                if (arr.indexOf(str) === -1) {
                                     arr.push(str);
                                 }
                                 return;
@@ -129,78 +142,71 @@
                             }
                         }
 
-                        function removeEmptyValues(node) {
+                        function preProcessTree(node, removeEmptyValues, normalizeWords) {
                             if(node && node instanceof Array) {
                                 var newArray = [];
                                 node.forEach(function(val, idx, array) {
                                     /*jshint unused: false*/    
-                                    var newVal = removeEmptyValues(val);
-                                    if (newVal) {
+                                    var newVal = preProcessTree(val, removeEmptyValues, normalizeWords);
+                                    if (!removeEmptyValues || newVal) {
                                         newArray.push(newVal);
                                     }   
                                 });
-                                if (newArray.length > 0) {
+                                if (!removeEmptyValues || newArray.length > 0) {
                                     return newArray;
                                 } else {
                                     return null;
                                 }
                             } else if (node && typeof node === 'object') {
+                                var nodeProperties = Object.getOwnPropertyNames(node);
+                                /*
+                                    XML to JSON converts XML arrays specified as:
+                                        <e> <a>text</a> <a>text</a> </e>	
+                                    to:
+                                        "e": { "a": ["text", "text"] }
+                                    When "a" is the only property of its object we would rather have the
+                                    following for display (because "a" doesn't contain any meaningful information
+                                    for the user):
+                                        "e": ["text", "text"]
+                                    This conditional converts the former to the latter for 
+                                    cases where "a" is the only property of its object.
+                                 */
+                                if (nodeProperties.length === 1 && node[nodeProperties[0]] instanceof Array) {
+                                    return preProcessTree(node[nodeProperties[0]], removeEmptyValues, normalizeWords);
+                                }
                                 var newObj = {};
-                                Object.getOwnPropertyNames(node).forEach(function(key, idx, array) {
+                                nodeProperties.forEach(function(key, idx, array) {
                                     /*jshint unused: false*/   
-                                    var newVal = removeEmptyValues(node[key]);
-                                    if (newVal) {
-                                        newObj[key] = newVal;
+                                    var newKey = key;
+                                    if (normalizeWords) {
+                                        //first replace all underscores in string with a blank
+                                        newKey = key.replace(/_/gi, function(txt) {
+                                            return ' ';
+                                        }); 
+                                        //normalize to first letter cap, rest lower for all words in the string.
+                                        newKey = newKey.replace(/\w\S*/g, function(txt) {
+                                            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                                        });
+                                    }
+                                    var newVal = preProcessTree(node[key], removeEmptyValues, normalizeWords);
+                                    if (!removeEmptyValues || newVal) {
+                                        newObj[newKey] = newVal;
                                     }
                                 });
                                 return newObj;
                             } else {
                                 if (node && ((typeof node === 'string' && node.length > 0) || (typeof node === 'boolean') || (typeof node === 'number'))) {
+
+                                    if (normalizeWords && typeof node === 'string') {
+                                        return node.replace(/\w\S*/g, function(txt) {
+                                            return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                                        });
+                                    }
                                     return node;
                                 } else {
                                     return null;
                                 }
                             }
-                        }
-
-                        if (scope.removeEmptyValues) {
-                            scope.fullTree = removeEmptyValues(scope.fullTree);
-                        }
-
-                        function normalizeWords(node) {
-                            if(node && node instanceof Array) {
-                                var newArr = [];
-                                node.forEach(function(val, idx, array) {
-                                    /*jshint unused: false*/    
-                                    newArr.push(normalizeWords(val));
-                                });
-                                return newArr;
-                            } else if (node && typeof node === 'object') {
-                                var newObj = {};
-                                Object.getOwnPropertyNames(node).forEach(function(key, idx, array) {
-                                    /*jshint unused: false*/  
-                                    //first replace all underscores in string with a blank
-                                    var newKey = key.replace(/_/gi, function(txt) {
-                                        return ' ';
-                                    }); 
-                                    //normalize to first letter cap, rest lower for all words in the string.
-                                    newKey = newKey.replace(/\w\S*/g, function(txt) {
-                                        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                                    });
-                                    newObj[newKey] = normalizeWords(node[key]); 
-                                });
-                                return newObj;
-                            } else if (typeof node === 'string') {
-                                return node.replace(/\w\S*/g, function(txt) {
-                                        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-                                });
-                            } else {
-                                return node;
-                            }
-                        }
-
-                        if (scope.normalizeWords) {
-                            scope.fullTree = normalizeWords(scope.fullTree);
                         }
                     },
                     template: '<script type="text/ng-template" id="node_renderer.html"><span ng-if="setControls(value)" ><span ng-click="toggleControls(value)" ng-if="showLabel(key)" ><span ng-class="value._iconClass"></span> <label class="key" ng-if="isString(key)">{{key}}</label><label class="delimiter" ng-if="isString(key) && isPrimitive(value)">: </label><label class="value" ng-if="isPrimitive(value)">{{value}}</label><label ng-if="isObject(value)">&nbsp;</label></span><div ng-if="value._childrenVisible"><div ng-repeat="(key, value) in value" ng-include="\'node_renderer.html\'" ng-class="getRepeatClass(value, false)"></div></div></span><\/script><div><input class="search-box"  ng-model="search" id="inputIcon" type="text" /> <i ng-class="searchIconClass"></i></div><strong  ng-show="search.length > 0" class="search-count">Matching results: {{ (displayTree|filter:search).length }}</strong><br><br><div ng-repeat="(key, value) in displayTree | filter:search" ng-include="\'node_renderer.html\'" ng-class="getRepeatClass(value, true)"></div>'
